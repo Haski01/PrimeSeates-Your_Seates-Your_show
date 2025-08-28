5004294bc847c6fd59bb7cc315d8c91406120128
Asad <asadshah1066@gmail.com>
2025-08-29 00:04:13 +0530

    PrimeSeates-project setup and build home page and navbar

---
+++ client/notes.md
@@ -0,0 +1,4 @@
+## Client side setup
+
+1. `create vite app` : npm install vite@latest
+2. 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import FeaturedSection from "../components/FeaturedSection";
+import HeroSection from "../components/HeroSection";
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <FeaturedSection />
+    </>
+  );
+};
+
+export default Home;
